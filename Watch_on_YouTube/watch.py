c8f0822252848bd5a156d3e6a35a9e806dc7728e
Giorgi Abashidze <giorgi.abashidze2@gmail.com>
2023-05-12 00:10:33 +0400

    improved watch.py - updated pattern in validate function.

--- Watch_on_YouTube/watch.py
+++ Watch_on_YouTube/watch.py
@@ -2,7 +2,6 @@ import re
 
 def main():
     s = input("HTML: ")
-   
     src = parse(s)
     print(validate(src))
 
@@ -13,15 +12,16 @@ def parse(s):
         if "src" in element:
             src = element
             return src.split("\"")[1]
-    return None
+    return False
 
 
 def validate(src):
-    if re.search(r"^(http){1}s?:?(\/\/)+(www)?\.(youtube)?\.+(com)?\/+(embed)+\/+[a-zA-Z0-9]*$", src):
+    if re.search(r"^(http){1}s?:?(\/\/)?(www)?\.?(youtube)?\.+(com)?\/+(embed)+\/+[a-zA-Z0-9]*$", src):
         unique = src.split("embed")[1]
-        return "https://youtu.be" + unique
+        http = "https://youtu.be"
+        return http + unique
     else:
-        None
+        False
 
 if __name__ == "__main__":
     main()
